587f7ea5aede214328d89f898756de83ba4c22a6
Chansoo Han <devhan1207@gmail.com>
2023-04-05 15:06:16 -0400

    letterPositions.js with test

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      results[sentence[i]] ? results[sentence[i]].push(i) : results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒 Assertion failed: ${actual} !== ${expected}`);
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertArraysEqual(letterPositions("hello").e, [1]);
