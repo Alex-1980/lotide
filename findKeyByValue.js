58120c148fd2663a0d3ce30b3697165193ae3533
Chansoo Han <devhan1207@gmail.com>
2023-04-05 16:11:25 -0400

    findKeyByValue.js with test

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  const keysArray = Object.keys(obj);
+  for(let item of keysArray) {
+    if(obj[item] === value) {
+      return item
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
