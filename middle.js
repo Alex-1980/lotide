d1d623080e012e9da9a34c66150a07ae01d0a058
Chansoo Han <devhan1207@gmail.com>
2023-04-05 09:41:14 -0400

    middle.js with test

---
+++ middle.js
@@ -0,0 +1,36 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒 Assertion failed: ${actual} !== ${expected}`);
+  }
+}
+
+const middle = function (array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
+  }
+}
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
