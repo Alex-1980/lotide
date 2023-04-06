1464cf9c321cb8e447564ca612460912f2a0a987
Chansoo Han <devhan1207@gmail.com>
2023-04-06 13:38:26 -0400

    findKey.js with test

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, cb) => {
+  for(let key in obj) {
+    if(cb(obj[key])) {
+      return key
+    }
+  }
+}
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
