17f7e2489009757c3654eb769b2ffc29367145c9
Chansoo Han <devhan1207@gmail.com>
2023-04-09 23:25:55 -0400

    refactoredAssertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,10 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
 const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
+  if (actual, expected) {
     console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
   } else {
     console.log(`😒😒😒 Assertion failed: ${actual} !== ${expected}`);
   }
 }
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = assertArraysEqual;
